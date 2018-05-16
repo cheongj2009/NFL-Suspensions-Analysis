2cac78f9f7e0d5398ccea73e4f913598d8df7201
James Cheong <cheongj2009@gmail.com>
2018-05-15 22:14:13 -0400

    Add files via upload

--- year_frequency.py
+++ year_frequency.py
@@ -33,3 +33,34 @@ unique_games = set(unique_games)
 # Display unique_teams and unique_games
 print(unique_teams)
 print(unique_games)
+
+
+# Create class for containing row information
+class Suspension:
+    def __init__(self, row):
+        self.row = row
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        self.year = row[5]
+
+# Example instantiation of the class
+third_suspension = Suspension(nfl_suspensions[2])
+
+# Create revised class for containing row information
+class SuspensionNew():
+    def __init__(self, row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        try:
+            self.year = int(row[5])
+        except Exception:
+            self.year = 0
+
+    def get_year(self):
+        return (self.year)
+
+# Test out SuspensionNew class using 23rd row in data
+missing_year = SuspensionNew(nfl_suspensions[22])
+twenty_third_year = missing_year.get_year()
